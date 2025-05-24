4088a1478b6b954a6447a0635a2b3f1610cda84e
danielleit241 <danielleee241@gmail.com>
2025-05-25 00:49:07 +0700

    Refactor room management functionality and UI
    - Simplified room retrieval in `RoomRepo.cs` and added `SearchByDesOrPrice` method.
    - Updated startup URI in `App.xaml` to launch `RoomWindow.xaml`.
    - Enhanced `RoomDetailsWindow.xaml` with new layout, labels, and event handlers for better user interaction.
    - Improved `RoomDetailsWindow.xaml.cs` to manage room editing and data population.
    - Modified `RoomWindow.xaml` to include event handlers and updated data grid columns.
    - Expanded `RoomWindow.xaml.cs` with methods for room creation, updating, deletion, and searching.
    - Introduced `RoomService.cs` for business logic related to room operations.
    - Added `RoomTypeService.cs` for managing room type retrieval.
    - Created `RoomTypeRepo.cs` for data access to room types.

---
+++ FUMiniHotelManagement.BLL/Services/RoomTypeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FUMiniHotelManagement.DAL.Entities;
+using FUMiniHotelManagement.DAL.Repositories;
+
+namespace FUMiniHotelManagement.BLL.Services
+{
+    public class RoomTypeService
+    {
+        private RoomTypeRepo _repo = new();
+        public List<RoomType> GetRoomTypes()
+        {
+            return _repo.GetAll();
+        }
+    }
+}
